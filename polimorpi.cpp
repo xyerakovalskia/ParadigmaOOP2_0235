c06c25eb00a1f3cb36a8ca5c84828047fb1f962c
xyerakovalskia <xyera123987@gmail.com>
2026-05-18 18:38:52 +0700

    class joko

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -8,3 +8,10 @@ public:
     //     cout<<"Pesan dari seseorang"<<endl;
     // }
 };
+
+class joko :public seseorang {
+public:
+    void pesan() {
+        cout << "Pesan dari joko" << endl;
+    }
+};
