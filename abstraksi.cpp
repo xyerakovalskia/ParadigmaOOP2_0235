ce79ff993eb81e1e0359575b4e4037d0bccfc0fc
xyerakovalskia <xyera123987@gmail.com>
2026-05-18 19:00:17 +0700

    menambahkan calss public di class abstraksi

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -3,3 +3,17 @@ using namespace std;
 
 class AbstraksiKlas {
 private: string x, y;
+
+public:
+    // method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+    //menampilkan nilai
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
