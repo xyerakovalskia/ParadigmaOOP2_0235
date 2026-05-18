78ddc9e642c2cac7c05d2cd1e6280d704f61fa30
xyerakovalskia <xyera123987@gmail.com>
2026-05-18 18:55:36 +0700

    menambahkan class perkerja dan pelajar

--- solvemultiwaris.cpp
+++ solvemultiwaris.cpp
@@ -11,3 +11,21 @@ public:
         cout << "orang dibuat dengan umur " << umur << "\n" << endl;
     }
 };
+
+class pekerja : public orang {
+public:
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
+
+class pelajar : public orang {
+public:
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
