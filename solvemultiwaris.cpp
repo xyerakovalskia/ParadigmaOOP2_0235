0f5cbba8d6752aa8a0fb868ef2133ca3f2890334
xyerakovalskia <xyera123987@gmail.com>
2026-05-18 18:56:04 +0700

    menambahkan class budi dan int main

--- solvemultiwaris.cpp
+++ solvemultiwaris.cpp
@@ -29,3 +29,18 @@ public:
         cout << "pelajar dibuat\n" << endl;
     }
 };
+
+class budi : public pekerja, public pelajar {
+public:
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur)
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
+
+int main() {
+    budi a(12);
+    return 0;
+}
