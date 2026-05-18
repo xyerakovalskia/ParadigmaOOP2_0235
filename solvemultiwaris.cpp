bdbbac1cfb34e2cedfc2800d2be789850136589e
xyerakovalskia <xyera123987@gmail.com>
2026-05-18 18:54:42 +0700

    menambahkan class orang

--- solvemultiwaris.cpp
+++ solvemultiwaris.cpp
@@ -11,36 +11,3 @@ public:
         cout << "orang dibuat dengan umur " << umur << "\n" << endl;
     }
 };
-
-class pekerja : public orang {
-public:
-    pekerja(int pUmur) :
-        orang(pUmur)
-    {
-        cout << "pekerja dibuat\n" << endl;
-    }
-};
-
-class pelajar : public orang {
-public:
-    pelajar(int pUmur) :
-        orang(pUmur)
-    {
-        cout << "pelajar dibuat\n" << endl;
-    }
-};
-
-class budi : public pekerja, public pelajar {
-public:
-    budi(int pUmur) :
-        pekerja(pUmur),
-        pelajar(pUmur)
-    {
-        cout << "Budi dibuat\n" << endl;
-    }
-};
-
-int main() {
-    budi a(12);
-    return 0;
-}
