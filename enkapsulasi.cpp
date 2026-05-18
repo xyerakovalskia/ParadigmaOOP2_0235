fdc7e3739e205688d350f4eb0ee64b4f7aadc4b4
xyerakovalskia <xyera123987@gmail.com>
2026-05-18 18:57:57 +0700

    menambahkan class public remotelampu

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -4,3 +4,11 @@ using namespace std;
 class remoteLampu {
 private:
     string saklarNo[5];
+public:
+    void setSaklarNo(int i, string value) {
+        saklarNo[i] = value;
+    }
+    string getSaklarNo(int i) {
+        return saklarNo[i];
+    }
+};
