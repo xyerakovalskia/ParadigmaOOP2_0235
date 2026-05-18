9e91ecec64706fb5096af82608c5e428cdbf02fb
xyerakovalskia <xyera123987@gmail.com>
2026-05-18 19:03:54 +0700

    menambhkan class orang dan class pekerja

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -19,30 +19,3 @@ public:
         cout << "pekerja dibuat\n" << endl;
     }
 };
-
-class pelajar : virtual public orang {
-public:
-    pelajar(int pUmur) :
-        orang(pUmur)
-    {
-        cout << "pelajar dibuat\n" << endl;
-    }
-};
-
-class budi : public pekerja, public pelajar {
-public:
-
-    budi(int pUmur) :
-        pekerja(pUmur),
-        pelajar(pUmur),
-        orang(pUmur) //hal ini dapat dilakukan jika menggunakan virtual
-    {
-        cout << "Budi dibuat\n" << endl;
-    }
-};
-
-int main() {
-    budi a(12);
-
-    return 0;
-}
